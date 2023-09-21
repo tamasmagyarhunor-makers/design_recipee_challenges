76fcb1d47d7740b33acbacc4f3cdcec649966c60
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-09-21 22:52:55 +0100

    Update comment

--- lib/calculate_rpm.py
+++ lib/calculate_rpm.py
@@ -3,7 +3,7 @@ import math
 def calculate_rpm(string):
     WORD_PER_MINUTE = 200
 
-    # 1. split number of words with 200 to see how many minutes it will take => might return float
+    # 1. divide number of words with 200 to see how many minutes it will take => might return float
     # 2. since it can return float, we have to use math.ceil so eg. 1.2 becomes 2
     minutes = math.ceil(len(string.split()) / WORD_PER_MINUTE) 
     if minutes <= 1:
