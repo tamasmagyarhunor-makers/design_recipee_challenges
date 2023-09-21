ee8c68f2714ada94d714e897a6eab9418aee6b9b
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-09-21 22:46:50 +0100

    Refactor calculate_rpm

--- lib/calculate_rpm.py
+++ lib/calculate_rpm.py
@@ -1,9 +1,11 @@
+import math
+
 def calculate_rpm(string):
     WORD_PER_MINUTE = 200
-    
-    words = len(string.split())
-    if words <= 200:
+
+    minutes = math.ceil(len(string.split()) / WORD_PER_MINUTE)
+    if minutes <= 1:
         return "1 minute"
     else:
-        return "2 minutes"
+        return f"{minutes} minutes"
     
