c6dfd2894abf3e709e980339dbd3f918b520d034
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-09-22 10:17:39 +0100

    Refactor

--- lib/calculate_rpm.py
+++ lib/calculate_rpm.py
@@ -2,13 +2,14 @@ import math
 
 def calculate_rpm(string):
     WORD_PER_MINUTE = 200
-    if len(string) < 1:
-        raise Exception("Can't estimate reading time for empty text")
-
     # 1. divide number of words with 200 to see how many minutes it will take => might return float
     # 2. since it can return float, we have to use math.ceil so eg. 1.2 becomes 2
     minutes = math.ceil(len(string.split()) / WORD_PER_MINUTE) 
-    if minutes <= 1:
+    
+    if minutes == 0.0:
+        raise Exception("Can't estimate reading time for empty text")
+    elif minutes <= 1:
+    
         return "1 minute"
     else:
         return f"{minutes} minutes"
